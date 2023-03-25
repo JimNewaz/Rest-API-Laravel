29e68f885b373d9de784ea9b5dac04a64a664c70
Sayed-Nur-E-Newaz <miznewaz@gmail.com>
2023-03-26 02:09:26 +0600

    DB migrated with factory and seeders

--- database/factories/InvoicesFactory.php
+++ database/factories/InvoicesFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Factories;
-
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +16,13 @@ class InvoicesFactory extends Factory
      */
     public function definition(): array
     {
+        $status = $this->faker->randomElement(['B', 'P', 'V']);
         return [
-            //
-        ];
+            'customer_id' => Customer::factory(), 
+            'amount' => $this->faker->numberBetween(100, 20000),
+            'status' => $status,
+            'billed_date' => $this->faker->dateTimeThisDecade(),
+            'paid_date' => $status == 'P' ? $this->faker->dateTimeThisDecade() : NULL
+         ];
     }
 }
